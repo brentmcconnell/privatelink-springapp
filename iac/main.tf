d037803d6fa46a111fc42d27daaa46ce29b6f2ec
brentmcconnell <brmcconn@microsoft.com>
2023-05-08 09:43:43 -0400

    added subnet integration

--- iac/main.tf
+++ iac/main.tf
@@ -327,6 +327,11 @@ resource "azurerm_subnet" "integrationsubnet" {
   }
 }
 
+resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
+  app_service_id  = azurerm_linux_web_app.backwebapp.id
+  subnet_id       = azurerm_subnet.integrationsubnet.id
+}
+
 # Create EndpointSubnet
 resource "azurerm_subnet" "endpointsubnet" {
   name                 = "endpointsubnet"
